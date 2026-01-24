ff3340edfe6ebb66f97a6e135da5de7cfa9e9a46
Senator <senatorcox90@gmail.com>
2026-01-24 09:42:39 +0000

    Resolving prod error

--- app/account/profile/error.js
+++ app/account/profile/error.js
@@ -3,11 +3,9 @@
 export default function Error({ error, reset }) {
   return (
     <main className="flex justify-center items-center flex-col gap-6">
-      <h1 className="text-3xl font-semibold">
-        Something went wrong!EEEEEEEEEEEEEEEEEEE
-      </h1>
+      <h1 className="text-3xl font-semibold">Something went wrong!</h1>
       <p className="text-lg">{error.message}!</p>
-
+      DDDDDDDDDDDDDDDDDDDDDDDDDDDDD
       <button
         onClick={reset}
         className="inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg"
