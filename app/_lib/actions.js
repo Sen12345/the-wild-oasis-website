d87f1bb17b5767141e7f613860da8309f7c29998
Senator <senatorcox90@gmail.com>
2026-01-24 08:31:38 +0000

    Add error boundary to guest area

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -18,7 +18,7 @@ export async function updateGuest(formData) {
 
   const updateData = { nationality, countryFlag, nationalID };
 
-  const { data, error } = await supabase
+  const { data } = await supabase
     .from("guests")
     .update(updateData)
     .eq("id", session.user.guestId);
