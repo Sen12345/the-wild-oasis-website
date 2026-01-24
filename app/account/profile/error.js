ba6494d77414fbb3e3e0c53ad0e2874e8e619981
Senator <senatorcox90@gmail.com>
2026-01-24 08:21:01 +0000

    Add error boundary to guest area

--- app/account/profile/error.js
+++ app/account/profile/error.js
@@ -12,7 +12,7 @@ export default function Error({ error, reset }) {
         onClick={reset}
         className="inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg"
       >
-        Try again
+        Try again later
       </button>
     </main>
   );
